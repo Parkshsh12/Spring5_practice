a1381a5a959d24e3edc7d3bb85cf9724859664c8
Parkshsh12 <krtmdgus2@naver.com>
2022-06-21 11:06:51 +0900

    2022-06-21 chapter06 실습

--- sp6-chap6/src/main/java/main/Main.java
+++ sp6-chap6/src/main/java/main/Main.java
@@ -7,15 +7,15 @@ import org.springframework.context.support.AbstractApplicationContext;
 
 import config.AppCtx_2;
 import spring.Client;
+import spring.Client2;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx_2.class);
 		
-		Client client = ctx.getBean(Client.class);
-		client.send();
-		
+		Client2 client2 = ctx.getBean(Client2.class);
+		client2.send();
 		ctx.close();
 	}
 }
