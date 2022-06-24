366cf2cfc70b7e06db32d3ce310855cff8e2534e
Parkshsh12 <krtmdgus2@naver.com>
2022-06-24 09:29:21 +0900

    2022-06-24 chapter07 실습

--- sp6-chap7/src/main/java/main/MainAspectWithCache.java
+++ sp6-chap7/src/main/java/main/MainAspectWithCache.java
@@ -10,10 +10,10 @@ public class MainAspectWithCache {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtxWithCache.class);
 		
 		Calculator cal = ctx.getBean("calculator", Calculator.class);
-		cal.factorial(7);
-		cal.factorial(7);
-		cal.factorial(5);
-		cal.factorial(5);
+		cal.factorial(10);
+		cal.factorial(10);
+		cal.factorial(8);
+		cal.factorial(8);
 		ctx.close();
 	}
 }
