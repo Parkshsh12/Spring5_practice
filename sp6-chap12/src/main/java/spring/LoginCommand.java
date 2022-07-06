4d8f2f290eaf0c50c476742987087d49ca1fa997
Parkshsh12 <krtmdgus2@naver.com>
2022-07-06 11:36:21 +0900

    2022-07-06 chapter12 실습

---
+++ sp6-chap12/src/main/java/spring/LoginCommand.java
@@ -0,0 +1,34 @@
+package spring;
+
+public class LoginCommand {
+	private String id;
+	private String email;
+	private String password;
+	private String loginType;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLoginType() {
+		return loginType;
+	}
+	public void setLoginType(String loginType) {
+		this.loginType = loginType;
+	}
+	
+	
+}
